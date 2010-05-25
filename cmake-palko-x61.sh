8b9099e7a85b98e7373f1b9018c912965e497200
Nicholas Palko <npalko@tepper.cmu.edu>
2010-05-25 17:40:50 -0500

    udated to new python location

--- cmake-palko-x61.sh
+++ cmake-palko-x61.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-protoc --cpp_out=src --python_out=src --proto_path=protobuf protobuf/*.proto
+protoc --cpp_out=src --proto_path=protobuf protobuf/*.proto
+protoc --python_out=python/urpc --proto_path=protobuf protobuf/*.proto
 cd build && cmake .. \
   -DCMAKE_MODULE_PATH="C:/Users/Nicholas Palko/Documents/Development/cmake-modules" \
   -DZMQ_INCLUDE_DIR="C:/Users/Nicholas Palko/Documents/Development/zeromq2/include" \
