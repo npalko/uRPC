be030d6d930378a7c7501b5653544b7843953a42
Nicholas Palko <npalko@tepper.cmu.edu>
2010-05-25 12:44:58 -0500

    added example cmake wrapper

---
+++ cmake-palko-x61.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+protoc --cpp_out=src --python_out=src --proto_path=protobuf protobuf/*.proto
+cd build && cmake .. \
+  -DCMAKE_MODULE_PATH="C:/Users/Nicholas Palko/Documents/Development/cmake-modules" \
+  -DZMQ_INCLUDE_DIR="C:/Users/Nicholas Palko/Documents/Development/zeromq2/include" \
+  -DZMQ_LIBRARY="C:/Users/Nicholas Palko/Documents/Development/zeromq2/lib" \
+  -DPROTOBUF_INCLUDE_DIR="C:/Users/Nicholas Palko/Documents/Development/protobuf/src" \
+  -DPROTOBUF_LIBRARY="C:/Users/Nicholas Palko/Documents/Development/protobuf/vsprojects/Release" \
+  -DMATLAB_INCLUDE_DIR="C:/Program Files/MATLAB/R2009a/extern/include" \
+  -DMATLAB_LIBRARY="C:/Program Files/MATLAB/R2009a/extern/lib/win32/microsoft"
