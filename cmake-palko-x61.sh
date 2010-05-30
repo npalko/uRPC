15030b3c50d1c3d177441083fb653bd12512423c
Nicholas Palko <npalko@tepper.cmu.edu>
2010-05-29 23:14:59 -0500

    added boost

--- cmake-palko-x61.sh
+++ cmake-palko-x61.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# cmake --build build/ --config Release
 protoc --cpp_out=src --python_out=python/urpc --proto_path=protobuf protobuf/*.proto
+protoc --cpp_out=examples/clientcpp --python_out=examples/serverpython --proto_path=examples/protobuf examples/protobuf/*.proto
 cd build && cmake .. \
   -DCMAKE_MODULE_PATH="C:/Users/Nicholas Palko/Documents/Development/cmake-modules" \
+  -DBOOST_ROOT="C:/Users/Nicholas Palko/Documents/Development/boost" \
   -DZMQ_INCLUDE_DIR="C:/Users/Nicholas Palko/Documents/Development/zeromq2/include" \
   -DZMQ_LIBRARY="C:/Users/Nicholas Palko/Documents/Development/zeromq2/lib" \
   -DPROTOBUF_INCLUDE_DIR="C:/Users/Nicholas Palko/Documents/Development/protobuf/src" \
