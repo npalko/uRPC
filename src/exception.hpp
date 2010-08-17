967a07b5528d3797d34dda94345471b8cc2123f5
Nicholas Palko <npalko@tepper.cmu.edu>
2010-08-17 19:47:26 -0400

    moved to header-only exceptions

--- src/exception.hpp
+++ src/exception.hpp
@@ -2,8 +2,29 @@
 //
 //
 
+#ifndef URPC_EXCEPTION_HPP
+#define URPC_EXCEPTION_HPP
 
 #include <stdexcept>
 
+namespace urpc {
 
+class NoDnsServerFound : public std::runtime_error {
+  public:
+    explicit NoDnsServerFound (const std::string& what)
+      : std::runtime_error (what) {}
+};
+class NoSrvRecordFound : public std::runtime_error {
+  public:
+    explicit NoSrvRecordFound (const std::string& what)
+      : std::runtime_error (what) {}
+};
+class GlobError : public std::runtime_error {
+  public:
+    explicit GlobError (const std::string& what)
+      : std::runtime_error (what) {}
+};
 
+}
+
+#endif // URPC_EXCEPTION_HPP
