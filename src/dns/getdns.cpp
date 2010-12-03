0cd5b4840468f5a843b5aa47c39ab496d089d833
Nicholas Palko <npalko@tepper.cmu.edu>
2010-12-03 09:43:08 -0500

    added outline of calling *nix api

--- src/dns/getdns.cpp
+++ src/dns/getdns.cpp
@@ -15,6 +15,37 @@ namespace dns {
 
 std::vector<boost::asio::ip::address> getServer() {
 
+/*
+
+resolver.h
+-lresolv
+libresolv
+
+
+res_state
+  nscount
+  nsaddr_list[MAXNS]  - array of unsigned int
+
+
+
+
+unsigned int  ipAddress = 167840383;
+unsigned char octet[4]  = {0,0,0,0};
+ 
+for (i=0; i<4; i++)
+{
+    octet[i] = ( ipAddress >> (i*8) ) & 0xFF;
+}
+
+printf("%d.%d.%d.%d\n",octet[3],octet[2],octet[1],octet[0]);
+
+
+
+*/
+
+
+
+
   std::vector<boost::asio::ip::address> server;
   std::ofstream resolv;
   
