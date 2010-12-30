e2ded8388d52565722abbf89aac024252cce25de
Nicholas Palko <npalko@tepper.cmu.edu>
2010-12-30 12:06:10 -0600

    more streamlined use of cmake

--- cmake-palko-air.sh
+++ cmake-palko-air.sh
@@ -5,10 +5,6 @@ protoc --cpp_out=test/pb --proto_path=test/protobuf test/protobuf/*.proto
 cd build && cmake .. \
   -DCMAKE_MODULE_PATH="/Users/npalko/Development/cmake-modules" \
   -DBOOST_ROOT="/Users/npalko/Development/boost_1_45_0" \
-  -DZMQ_INCLUDE_DIR="/Users/npalko/Development/zeromq2/include" \
-  -DZMQ_LIBRARY=/usr/local/lib \
-  -DPROTOBUF_INCLUDE_DIR="/Users/npalko/Development/protobuf/src" \
-  -DPROTOBUF_LIBRARY=/usr/local/lib \
   -DMATLAB_INCLUDE_DIR="/Applications/MATLAB_R2009b.app/extern/include" \
   -DMATLAB_LIBRARY="/Applications/MATLAB_R2009b.app/extern/lib/maci64"
 
